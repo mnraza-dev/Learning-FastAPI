aac3f11f47ec607b66e58f0cdd674becbc693826
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-16 01:14:07 +0530

    Add endpoint to create a form and return a placeholder response

--- todo-api/main.py
+++ todo-api/main.py
@@ -2,11 +2,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/items")
-def list_items():
+async def list_items():
     return {"message": "List of Items"}
 
 @app.get("/items/{item_id}")
-def get_items(item_id:int):
+async def get_items(item_id:int):
     return {
         "message":item_id
+    }
+
+@app.post("/")
+async def create_form():
+    return{
+        "data":"All forms listed"
     }
