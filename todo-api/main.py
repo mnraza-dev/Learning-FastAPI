a8801c57b2e50fed69322952e653fa45b77b8f05
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-16 03:47:28 +0530

    Add endpoints for about information and user retrieval

--- todo-api/main.py
+++ todo-api/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
 app = FastAPI()
 
+@app.get("/about")
+async def about():
+    return{
+        "message":"About me"
+    }
+
+@app.get("/users/{user_id}")
+async def get_users(user_id:str):
+    return {
+        "message":user_id
+    }
 @app.get("/items")
 async def list_items():
     return {"message": "List of Items"}
